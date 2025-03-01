24e4088e52a4869e0e0ac83a340d3e55321f3f32
joseph.campos <josephcampos91@gmail.com>
2025-03-01 14:48:41 -0600

    test: update AppComponent and StudiesService test configurations

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { StudiesService } from './shared/services/studies.service';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  let studiesServiceSpy: jasmine.SpyObj<StudiesService>;
+
+  beforeEach(() => {
+    studiesServiceSpy = jasmine.createSpyObj('StudiesService', ['GetStudies']);
+    studiesServiceSpy.GetStudies.and.returnValue(of([]));
+
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+      providers: [
+        { provide: StudiesService, useValue: studiesServiceSpy }
+      ]
+    });
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +26,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'curriculum-vitae'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('curriculum-vitae');
-  });
-
-  it('should render title', () => {
+  it('should initialize and log message', () => {
+    const consoleSpy = spyOn(console, 'log');
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('curriculum-vitae app is running!');
+    
+    expect(consoleSpy).toHaveBeenCalledWith('AppComponent initialized');
   });
 });
--- src/app/shared/services/studies.service.spec.ts
+++ src/app/shared/services/studies.service.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { StudiesService } from './studies.service';
 
 describe('StudiesService', () => {
   let service: StudiesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [StudiesService]
+    });
     service = TestBed.inject(StudiesService);
   });
 
